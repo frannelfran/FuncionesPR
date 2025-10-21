27408681b772be77119815acd6931010b5bd90fc
Franco Alla <frannelfran@gmail.com>
2025-10-21 16:46:51 +0100

    feat: declaración de la clase abstracta para las funciones primitivas recursivas

---
+++ src/funcionPR/funcionPR.h
@@ -0,0 +1,21 @@
+#ifndef FUNCIONESPR_H
+#define FUNCIONESPR_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+static int llamadas = 0;
+
+class FuncionPR {
+  public:
+    // Constructor y destructor
+    FuncionPR() = default;
+    ~FuncionPR() = default;
+
+    // Métodos
+    virtual int evaluar(const vector<int>& args) = 0;
+};
+
+#endif // FUNCIONESPR_H
