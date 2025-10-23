95661d26ef8dffc1a00dc6f8cb7f4d191f157c02
Franco Alla <frannelfran@gmail.com>
2025-10-23 21:38:43 +0100

    feat: Programa principal completado

--- src/main.cc
+++ src/main.cc
@@ -1,28 +1,20 @@
 #include "funcionPR/potencia/potencia.h"
-
-void mostrarResultado(int base, int exponente, int resultado, int llamadas) {
-  cout << "Base: " << base << endl;
-  cout << "Exponente: " << exponente << endl;
-  cout << " => Resultado: " << resultado << endl;
-  cout << " => Llamadas recursivas: " << llamadas << endl;
-  cout << "-------------------------" << endl;
-}
+#include "tools/tools.h"
 
 int main() {
   cout << "Programa que evalua la función primitiva recursiva potencia: NxN -> N\n" << endl;
+  cout << "Pulsar Ctrl+C para salir.\n" << endl;
   try {
     while (true) {
       cout << "Ingrese la base (entero no negativo): ";
       int base;
       cin >> base;
+      comprobarEntrada(base);
       cout << "Ingrese el exponente (entero no negativo): ";
       int exponente;
       cin >> exponente;
+      comprobarEntrada(exponente);
       cout << endl;
-      if (base < 0 || exponente < 0) {
-        cout << "Por favor, ingrese enteros no negativos." << endl;
-        continue;
-      }
       
       FuncionPR::contadorLlamadas = 0; // Reiniciar el contador
       Potencia p;
